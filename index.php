5232680f23a54a1fe372230daf5fe8369fb7d747
derkiab <110829988+derkiab@users.noreply.github.com>
2022-11-14 20:17:38 -0300

    guest

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-
-
 <?php
 
     session_start();
 
     if(!isset($_SESSION['rol'])){
-        header('location: login.php');
+        header('location: guest/home.php');
     }else{
         if($_SESSION['rol'] != 2 || $_SESSION['rol'] != 1){
             header('location: login.php');
@@ -14,8 +12,6 @@
         }
           header('location: pages\home.php');
     }
-
-
 
 
 ?>
