8d4d1d006cec734b134d824fe7c69e4866492d9f
Fhil48 <diegorsk2@gmail.com>
2022-12-20 04:46:30 -0300

    notice and events

--- user/templates/footer.php
+++ user/templates/footer.php
@@ -1,7 +1,6 @@
 <footer class="mt-auto">
         <div class="pt-5" style="background-color:#486FAF;">
         <div class="card-body">
-           
             <div class="container text-star">
                 <div class="row">
                     <div class="col">
@@ -39,7 +38,6 @@
                         <P> DIRECCION: Calle Bernardo O`Higgins 525, Concepción, Bío Bío</P>
                         <P> TELEFONO: (41) 226 6500</P>
                     </div>
-
                 </div>
             </div>
         </div>
